70cedfe71e7bb56107a34f0874adff4759341ddb
ideahitme <yerken.tussupbekov@zalando.de>
2016-11-01 01:34:07 +0100

    swagger, godep

---
+++ restapi/doc.go
@@ -0,0 +1,19 @@
+/*Package restapi Erebus
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /
+    Version: 0.1.0
+
+    Consumes:
+    - application/github.erebus+json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
